466f50a0ded2c0b1367a3065745962c379f64ce9
philcannon <109622321+philcannon@users.noreply.github.com>
2025-02-24 13:22:11 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,25 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: { y: 1 },
+            debug: true // Disable in production
+        }
+    },
+    scene: {
+        init: init,
+        preload: preload,
+        create: create,
+        update: update
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+};
 
+const game = new Phaser.Game(config);
